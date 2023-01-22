866c97b07571d42bbc7de966cd1b2e4edc5e1d16
ourTeam-webs <web43473@gmail.com>
2023-01-22 19:22:40 +0300

    carusel

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,4 @@
-// $("#ShowPassword").addEventListener("click", (e) => {
-//     if (e.target.classList.contains("shownow")) {
-//       e.target.classList.remove("shownow");
-//       e.target.classList.add("hidenow");
-//       $("#hidePassword").classList.remove("hidenow");
-//       $("#hidePassword").classList.add("shownow");
-  
-//       $("#modalpasswordinput").type = "text";
-//     }
-//   });
-//   $("#hidePassword").addEventListener("click", (e) => {
-//     if (e.target.classList.contains("shownow")) {
-//       e.target.classList.remove("shownow");
-//       e.target.classList.add("hidenow");
-//       $("#ShowPassword").classList.remove("hidenow");
-//       $("#ShowPassword").classList.add("shownow");
-  
-//       $("#modalpasswordinput").type = "password";
-//     }
-//   });
-  
-//   $("#closemodal").addEventListener("click", () => {
-//     $(".modal-window").style.display = "none";
-//   });
-//   $(".login").addEventListener("click", () => {
-//     $(".modal-window").style.display = "block";
-//   });
+
 
 const slides = document.querySelectorAll(".armchairs_card");
 let idx = 0;
@@ -64,3 +38,15 @@ function resetInterval() {
 
   interval = setInterval(run, 4000);
 }
+
+
+
+
+
+
+
+
+
+
+
+
